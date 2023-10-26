ab2c7ba1684b7dae4bf0dee7381f0a3ae7a5a60b
Jakiro12 <eclipsisnocte@gmail.com>
2023-10-26 03:18:15 -0300

    feat:Nuevos elementos y funcionalidades

--- Settings_intefaces/StatusAndSettings.js
+++ Settings_intefaces/StatusAndSettings.js
@@ -1,8 +1,28 @@
     const wheelColorTag=document.createElement('div')
+    const fireflyTag=document.createElement('div')
+    const progressBarTag=document.createElement('progress')
+    const btnSettignsTag=document.createElement('div')
+    const btnVolume=document.createElement('button')
+    const btnGear=document.createElement('button')
+
+    btnSettignsTag.appendChild(btnVolume)
+    btnSettignsTag.appendChild(btnGear)
     wheelColorTag.className='wheel_status'
+    fireflyTag.className='fire_fly_container'
+    progressBarTag.className='progess_bar'
+    btnSettignsTag.className='btn_settings_ui'
+    progressBarTag.setAttribute('max','100')
+    progressBarTag.setAttribute('min','0')
+    btnVolume.className='btn_volume'
+    btnGear.className='btn_volume'
+
 function addComponentsToHandleStatusAndSettings(){
 const parentElement=document.querySelector('.game_info_and_options_2')
     parentElement.appendChild(wheelColorTag)
+    parentElement.appendChild(fireflyTag)
+    parentElement.appendChild(progressBarTag)
+    parentElement.appendChild(btnSettignsTag)
+    progressBarTag.value='70'
 }
 
 export {addComponentsToHandleStatusAndSettings}
