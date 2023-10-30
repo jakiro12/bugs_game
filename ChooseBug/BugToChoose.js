be8785e56ebe2291ef3957600c3b741ff5f5dbc2
Jakiro12 <eclipsisnocte@gmail.com>
2023-10-30 03:06:38 -0300

    feat(bugChoose): styles y variable global

--- ChooseBug/BugToChoose.js
+++ ChooseBug/BugToChoose.js
@@ -1,11 +1,18 @@
+import { chromaticWheelColors } from "../GlobarVars/chromaticColors";
+
 const boxToShowCurrentBug=document.createElement('div')
 boxToShowCurrentBug.className='card_current_bug'
+const bugRequired=document.createElement('div')
+bugRequired.className='bug_to_pick'
+
 function handleBugToChoose (){
     const parentElement=document.querySelector('.header_game_options');
     const allBugsMoving=document.querySelectorAll('.bugs_styles')
     const chromaticElement=document.querySelector('.wheel_status')
     parentElement.appendChild(boxToShowCurrentBug)
-    console.log(allBugsMoving)
+    boxToShowCurrentBug.appendChild(bugRequired)
+    bugRequired.style.backgroundColor=`${chromaticWheelColors[0]}`
+    console.log(chromaticWheelColors)
     allBugsMoving[0].addEventListener('click',()=>{
         let colorName= getComputedStyle(allBugsMoving[0]).getPropertyValue('--set-colorbtn') //los estilos de ese insecto
         console.log('hola colorin  '+ colorName)
---
+++ GlobarVars/chromaticColors.js
@@ -0,0 +1,1 @@
+export const chromaticWheelColors=["red","blue","yellow"]
