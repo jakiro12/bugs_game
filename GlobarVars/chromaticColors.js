7ac399e63ca8ab322091cc7d376ed35fe8c7803c
Jakiro12 <eclipsisnocte@gmail.com>
2023-11-17 02:49:13 -0300

    fix:colores terciarios agregados

--- GlobarVars/chromaticColors.js
+++ GlobarVars/chromaticColors.js
@@ -1,11 +1,12 @@
 export const chromaticWheelColors=["red","blue","yellow"]
 export const secondarychromaticWheelColors=[]
+export const ternarychromaticWheelColors=[]
 export const pickTwoColors={
     color1:'',
     color2:''
 }
 export const colorCombinations = {
-    bluered: 'violet',
+    bluered: 'purple',
     redyellow: 'orange',
     blueyellow: 'green'
 };
--- Modals/CompleteLevel.js
+++ Modals/CompleteLevel.js
@@ -1,20 +1,24 @@
-import {  secondarychromaticWheelColors } from "../GlobarVars/chromaticColors"
+import {  secondarychromaticWheelColors, ternarychromaticWheelColors } from "../GlobarVars/chromaticColors"
 
 const modalBox=document.createElement('main')
 const modalContainer=document.createElement('article')
 const btnCloseModal=document.createElement('button')
+const parrfAboutLevel=document.createElement('p')
     modalContainer.className='alert'
     modalContainer.id='modal_level_info'
     modalBox.className='modal_box_information'
     btnCloseModal.innerText='cerrar'
+    parrfAboutLevel.className='alert_information'
 export function levelSuccesslful(){
     const wheelSettings=document.getElementById('wheel')
-    const secondaryColors=['violet','green','orange'] 
+    const secondaryColors=['purple','green','orange'] 
+    const ternaryColors=['blueviolet','orangered','chartreuse','teal','violet','magenta']
     const bugRequired=document.querySelector('.bug_to_pick')
     modalContainer.appendChild(modalBox)
     const gameView=document.getElementById('phaser-container')
     gameView.appendChild(modalContainer)
-    modalBox.innerText='nivel completado'
+    parrfAboutLevel.textContent='nivel 1 completado'
+    modalBox.appendChild(parrfAboutLevel)
     modalBox.appendChild(btnCloseModal)
 
     function subLevel_1(){
@@ -28,11 +32,30 @@ export function levelSuccesslful(){
             wheelSettings.className='wheel_status_sub_level_2'
         }
     }
+    function subLevel_2(){
+        ternarychromaticWheelColors.push(ternaryColors[0])
+        ternarychromaticWheelColors.push(ternaryColors[1])
+        ternarychromaticWheelColors.push(ternaryColors[2])
+        ternarychromaticWheelColors.push(ternaryColors[3])
+        ternarychromaticWheelColors.push(ternaryColors[4])
+        ternarychromaticWheelColors.push(ternaryColors[5])
+        modalContainer.className=''
+        if (secondarychromaticWheelColors.length > 0) {
+            bugRequired.style.backgroundColor = ternarychromaticWheelColors[0];
+            
+        }
 
+    }
     btnCloseModal.addEventListener('click',()=>{
         console.log(wheelSettings.classList)
         if(wheelSettings.classList[0] === 'wheel_status_sub_level_1'){
             subLevel_1()
+        }else if(wheelSettings.classList[0] === 'wheel_status_sub_level_2'){
+            console.log('cerrar nivel 2')
+            subLevel_2()
+            console.log(ternarychromaticWheelColors)
+        }else{
+            console.log('juego terminado')
         }
     })
 
